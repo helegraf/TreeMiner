145127f3ab04f2e72f11424dd9d22082453c63fb
Helena Graf <Graf.Helena@web.de>
2018-08-06 14:12:14 +0200

    Some more tests

--- src/test/java/treeminer/TreeMinerTest.java
+++ src/test/java/treeminer/TreeMinerTest.java
@@ -1,37 +1,55 @@
 package treeminer;
 
+import java.util.Arrays;
+
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TreeMinerTest {
-	
+
 	public static TreeMiner treeMiner;
-	
+
 	@Before
 	public void initializeTreeMiner() {
 		treeMiner = new TreeMiner();
 	}
-	
+
+	@Test
 	public void testContainsSubtree() {
 		TreeRepresentationUtils.containsSubtree("A B -1", "A");
 	}
-	
+
 	@Test
 	public void testAddNodeToPrefixLastNode() {
-		System.out.println(TreeRepresentationUtils.addNodeToTree("a b -1", new ImmutablePair<String,Integer>("c", 1)));
+		System.out.println(TreeRepresentationUtils.addNodeToTree("a b -1", new ImmutablePair<String, Integer>("c", 1)));
 	}
-	
+
 	@Test
 	public void testAddNodeToPrefixIntermediate() {
-		System.out.println(TreeRepresentationUtils.addNodeToTree("a b -1", new ImmutablePair<String,Integer>("c", 0)));
+		System.out.println(TreeRepresentationUtils.addNodeToTree("a b -1", new ImmutablePair<String, Integer>("c", 0)));
 	}
-	
+
+	@Test
 	public void testAddNodeToRoot() {
-		System.out.println(TreeRepresentationUtils.addNodeToTree("a", new ImmutablePair<String,Integer>("c", 0)));
+		System.out.println(TreeRepresentationUtils.addNodeToTree("a", new ImmutablePair<String, Integer>("c", 0)));
+	}
+
+	@Test
+	public void testAddNodeToLongTree() {
+		System.out.println(TreeRepresentationUtils.addNodeToTree("a b c -1 e -1 -1 -1",
+				new ImmutablePair<String, Integer>("d", 1)));
+	}
+
+	@Test
+	public void testFindSubtrees() {
+		System.out.println(treeMiner.findFrequentSubtrees(Arrays.asList("A B -1 C -1", "A", "A C -1", "A B D -1 -1"),1));
+		System.out.println(treeMiner.getFoundEquivalenceClasses());
 	}
 	
-	public void testAddNodeToLongTree() {
-		System.out.println(TreeRepresentationUtils.addNodeToTree("a b c -1 e -1 -1 -1", new ImmutablePair<String,Integer>("d", 1)));
+	public static void main (String [] args) {
+		TreeMinerTest test = new TreeMinerTest();
+		test.initializeTreeMiner();
+		test.testFindSubtrees();
 	}
 }
